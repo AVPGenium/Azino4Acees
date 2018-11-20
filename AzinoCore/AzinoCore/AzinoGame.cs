e46e9cd58207691b6a1aa82fea73455fa56171c2
ilyzhin <lyzhin-ivan@yandex.ru>
2018-11-20 13:15:58 +0500

    init core library

---
+++ AzinoCore/AzinoCore/AzinoGame.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace AzinoCore
+{
+    public class AzinoGame
+    {
+        public AzinoGame()
+        {
+            Balance = Rules.StartBalance;
+        }
+
+        public int Balance { get; private set; }
+    }
+
+    public static class Rules
+    {
+        static Rules()
+        {
+            StartBalance = 100;
+            SpinCost = 10;
+        }
+
+        public static int SpinCost { get; }
+        public static int StartBalance { get; }
+    }
+}
