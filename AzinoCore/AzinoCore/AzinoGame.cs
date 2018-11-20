7b419d26dffdec54c34c3b3a5b5153f289a55cfc
ilyzhin <lyzhin-ivan@yandex.ru>
2018-11-20 18:24:14 +0500

    core lib generating state

--- AzinoCore/AzinoCore/AzinoGame.cs
+++ AzinoCore/AzinoCore/AzinoGame.cs
@@ -7,9 +7,24 @@ namespace AzinoCore
         public AzinoGame()
         {
             Balance = Rules.StartBalance;
+            State = new int[] { 1, 2, 3, 4, 5 };
+        }
+
+        public void Spin()
+        {
+            if (Balance < Rules.SpinCost)
+                return;
+            Balance -= Rules.SpinCost;
+            Random r = new Random();
+            for(int i=0; i<5; ++i)
+            {
+                State[i] = r.Next(10);
+            }
+            Balance += Rules.GetDelta(State);
         }
 
         public int Balance { get; private set; }
+        public int[] State { get; private set; }
     }
 
     public static class Rules
@@ -20,7 +35,13 @@ namespace AzinoCore
             SpinCost = 10;
         }
 
+        public static int GetDelta(int[] state)
+        {
+            return 0;
+        }
+
         public static int SpinCost { get; }
         public static int StartBalance { get; }
     }
 }
+
